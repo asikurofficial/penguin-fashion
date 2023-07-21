126a346444e5baa2f66c4198d0ea6582789f408e
asikurofficial <asikurofficial@gmail.com>
2023-07-21 20:32:43 +0600

    haeder & banner

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      FontFamily: {
+        lato:['lato', 'sans-serif'],
+      },
+      extend: {},
+      
+    },
+    plugins: [],
+   }
